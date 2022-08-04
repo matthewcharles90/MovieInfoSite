6033738b83a3d691de200cf12151f0e31fd27e93
Matt Charles <matthewcharles90@gmail.com>
2022-08-04 15:38:55 -0400

    update script.js
    References

---
+++ script.js
@@ -0,0 +1,5 @@
+// Initial Referneces
+let movieName = document.getElementById("movie-title");
+let searchBtn = document.getElementById("search-btn");
+let result = document.getElementById("result");
+
